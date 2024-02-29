ac6ce57af6a65537c84e68fbc0655d892c85a53b
Imran Afzal <imranafzal02@gmail.com>
2024-02-29 19:23:25 +0500

    added app.css file to admin blade

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -23,7 +23,7 @@
     <!-- Daterange picker -->
     <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}">
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @livewireStyles
 
